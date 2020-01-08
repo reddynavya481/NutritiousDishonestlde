bbafe9cdc98abcb5db2c01cc9e38af58559e4f0d
reddynavya481 <reddynavya481@gmail.com>
2020-01-08 11:05:26 +0000

    modularize the application and added exports

---
+++ business.js
@@ -0,0 +1,13 @@
+function add(a,b)
+{
+  return a+b;
+}
+function sub(a,b)
+{
+  return a-b
+}
+function profit(a,b)
+{
+  return sub(a,b)
+}
+module.exports={add,sub,profit} 
--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-function add(a,b)
-{
-  return a+b;
-}
-function sub(a,b)
-{
-  return a-b
-}
-function profit(a,b)
-{
-  return sub(a,b)
-}
+d=require('./business')
 x=6
 y=7
-c=add(x,y);
-d=sub(x,y);
-e=profit(x,y)
-console.log(c+" "+d+" "+e);
+c=d.add(x,y);
+d=d.sub(x,y);
+// e=d.profit(x,y);
+console.log(c+" "+d+" ");
