976af0fb0c110fc50812e2bea2937b1b93e773a9
reddynavya481 <reddynavya481@gmail.com>
2020-01-08 10:50:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a,b)
+{
+  return a+b;
+}
+function sub(a,b)
+{
+  return a-b
+}
+function profit(a,b)
+{
+  return sub(a,b)
+}
+x=6
+y=7
+c=add(x,y);
+d=sub(x,y);
+e=profit(x,y)
+console.log(c+" "+d+" "+e);
